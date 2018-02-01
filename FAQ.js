2405c27e1c2cf0ca4ff3ddeae96f9fa37c0fe94f
ArmTrade96 <hr.armani1996@gmail.com>
2018-02-01 19:48:14 +0200

    FAQ

---
+++ FAQ.js
@@ -0,0 +1,60 @@
+/**
+ * Created by Арман on 29.01.2018.
+ */
+$('.plius').click(function () {
+    $(this).parent().next().slideToggle();
+    console.log($(this).parent().next());
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// background: #8FBC8F;
+// <div class="our-story">
+//<div class="container">
+//    <h2>Здесь скоро будет видос, посоны</h2>
+//</div>
+//</div>
+//<div class="token-usage">
+//    <div class="container">
+//    <h2>здесь скоро будут графики, посоны</h2>
+//</div>
+//</div>
+//<div class="team">
+//    <div class="container">
+//    <h2>Здесь скоро будут рожи команды, посоны</h2>
+//</div>
+//</div>
+//<div class="roadmap">
+//    <div class="container">
+//    <h2>Здесь скоро будет красивая графика, посоны</h2>
+//</div>
+//</div>
